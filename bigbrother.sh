197cc1610f9417a98854642689e62fdc99ef1c29
steven harradine <steven.harradine@telus.com>
2015-05-24 01:21:25 -0400

    refactor

--- bigbrother.sh
+++ bigbrother.sh
@@ -3,7 +3,6 @@ scriptsPath="scripts/"
 scripts=$( cd $scriptsPath && ls)
 
 for script in $scripts; do
-  command="./$scriptsPath$script"
   echo "Running $script"
-  sh $command
+  sh ./$scriptsPath$script
 done
