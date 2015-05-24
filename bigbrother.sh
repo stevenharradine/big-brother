2312840688372ec8db58619df6d73c8504018792
steven harradine <steven.harradine@telus.com>
2015-05-24 01:57:21 -0400

    run tasks on remote server via ssh

--- bigbrother.sh
+++ bigbrother.sh
@@ -2,7 +2,15 @@
 scriptsPath="scripts/"
 scripts=$( cd $scriptsPath && ls)
 
+host="example.com"
+
+ssh $host mkdir .bigbrother
+scp -r * $host:~/.bigbrother
+
 for script in $scripts; do
   echo "Running $script"
-  sh "./$scriptsPath$script"
+
+  ssh $host ~/.bigbrother/$scriptsPath$script
 done
+
+ssh $host rm -rf ~/.bigbrother
