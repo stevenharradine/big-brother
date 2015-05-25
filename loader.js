999b7fb732b26fbe9c971cbe16093378e73f6869
steven harradine <steven.harradine@telus.com>
2015-05-24 21:37:21 -0400

    hide all the OK messages when theres some alerts/warnings to only show relevant data

--- loader.js
+++ loader.js
@@ -1,18 +1,38 @@
-var path = "./" + process.argv[2];
+var path         = "./" + process.argv[2];
 
 process.stdin.setEncoding('utf8');
+
+Array.prototype.hasStatus = function (status) {
+    for (i = 0; i < this.length; i++) {
+    	var hasStatus = this[i].status == status;
+
+        if (hasStatus) return true;
+    }
+}
 
 process.stdin.on('readable', function() {
 	var chunk = process.stdin.read();
 
 	if (chunk !== null) {
-		var statuses = require (path)(chunk.trim());
+		var statuses       = require (path)(chunk.trim()),
+		    log            = "";
 
-		for (statusesIndex = 0; statusesIndex < statuses.length; statusesIndex++) {
-			var status  = statuses[statusesIndex].status,
-			    message = statuses[statusesIndex].message;
+		if (statuses.hasStatus ("ALERT") || statuses.hasStatus("Warning")) {
+			for (statusesIndex = 0; statusesIndex < statuses.length; statusesIndex++) {
+				var status        = statuses[statusesIndex].status,
+				    message       = statuses[statusesIndex].message;
 
-			console.log ("    " + status + ( message != undefined ? " " + message : ""));
+				if (status == "ALERT" || status == "Warning") {
+					log += "    " +
+					       status +
+					       ( message != undefined ? " " + message : "") +
+					       ( statusesIndex < statuses.length - 1 ? "\n" : "" );
+				}
+			}
+		} else {
+			log += "    OK";
 		}
+
+		console.log (log);
 	}
 });
