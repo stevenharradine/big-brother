8dc6c41e946dd062e5412528f8f0e69c2f99d523
steven harradine <steven.harradine@telus.com>
2015-05-24 22:22:17 -0400

    refact hasStatus :

--- loader.js
+++ loader.js
@@ -1,21 +1,19 @@
-var path         = "./" + process.argv[2];
+var path = "./" + process.argv[2];
 
 process.stdin.setEncoding('utf8');
-
-Array.prototype.hasStatus = function (status) {
-    for (i = 0; i < this.length; i++) {
-    	var hasStatus = this[i].status == status;
-
-        if (hasStatus) return true;
-    }
-}
 
 process.stdin.on('readable', function() {
 	var chunk = process.stdin.read();
 
 	if (chunk !== null) {
-		var statuses       = require (path)(chunk.trim()),
-		    log            = "";
+		var statuses = require (path)(chunk.trim()),
+		    log      = "";
+
+		statuses.hasStatus = function (status) {
+		    for (i = 0; i < this.length; i++)
+		    	if (this[i].status == status)
+		    		return true;
+		}
 
 		if (statuses.hasStatus ("ALERT") || statuses.hasStatus("Warning")) {
 			for (statusesIndex = 0; statusesIndex < statuses.length; statusesIndex++) {
