bdbb9baf4d5066bedb60de4a205c1afb1d172b30
Steven Harradine <stevenharradine@gmail.com>
2015-06-24 04:00:40 -0400

    add sql logging for disk usage

--- scripts-available/disk-usage/process.js
+++ scripts-available/disk-usage/process.js
@@ -2,6 +2,8 @@ module.exports = function (data) {
 	var lines    = data.chunk.split ("\n"),
 	    statuses = [];
 
+	data.mysql_connection.connect();
+
 	for (var linesIndex = 0; linesIndex < lines.length; linesIndex++) {
 		var this_line = lines[linesIndex].replace(/\s+/g, ' ').split(" "),
 		    path      = this_line[0],
@@ -16,9 +18,18 @@ module.exports = function (data) {
 				"status":"Warning",
 				"message":message
 		}) : statuses.push ({
-				"status":"OK"
+				"status":"OK",
+				"message":message
+		});
+
+		var sql = "INSERT INTO `big_brother`.`disk_usage` (`host`, `path`, `percent`) VALUES ('" + data.host + "', '" + path + "', '" + percent + "')";
+
+		data.mysql_connection.query(sql, function(err, rows, fields) {
+			if (err) throw err;
 		});
 	}
+	
+	data.mysql_connection.end();
 
 	return statuses;
 }
